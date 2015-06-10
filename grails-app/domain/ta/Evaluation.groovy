e6e14971f6415392954027f359ebb9ad28fe916e
leoschet <leoschettini2@gmail.com.com>
2015-06-09 22:32:10 -0300

    [#96617642] Criar assert de avaliação

--- grails-app/domain/ta/Evaluation.groovy
+++ grails-app/domain/ta/Evaluation.groovy
@@ -8,5 +8,47 @@ class Evaluation {
         title blank: false
     }
 
+	public Evaluation() {
+		title = null;
+		questions = new ArrayList<Question>()
+	}
+
 	public Question getQuestion(int index) { questions[index] }
+
+	public boolean equals(Evaluation ev) {
+		boolean equals = true;
+
+		if (this.title != ev.title){
+			equals = false
+		}
+
+		if (this.questions != null) {
+			if (ev.questions != null) {
+
+				if (this.questions.size() == ev.questions.size()){
+					for (int i = 0; i < questions.size(); i += 1) {
+
+						if (!this.questions[i].question.equals(ev.questions[i].question)) {
+							equals = false
+						}
+
+						if (!this.questions[i].answer.equals(ev.questions[i].answer)) {
+							equals = false
+						}
+
+						if (this.questions[i].alternatives.size() == ev.questions[i].alternatives.size()) {
+							equals = false
+						}
+					}
+				}
+
+			} else {
+				equals = false
+			}
+		} else {
+			equals = false
+		}
+
+		return equals
+	}
 }
