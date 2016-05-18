6ee00da8d9ba2ced51fc6d7eaffe6fa3c996a1dd
DLRibeiro <dlr4@cin.ufpe.br>
2016-05-18 20:58:06 -0300

    Ajustei minhas classes dominio

--- grails-app/domain/ta/Evaluation.groovy
+++ grails-app/domain/ta/Evaluation.groovy
@@ -4,9 +4,11 @@ class Evaluation {
     String origin;
     String value;
     Date applicationDate;
+    Criterion criterion;
     static constraints = {
-        criterion blank : false
         origin inList :["Test","Mini-Test","Form","Final"], blank: false
-        value inList :["MA","MPA","MANAA","--"], blank :false
+        value inList :["MA","MPA","MANA","--"], blank :false
+        criterion nullable : false
     }
+
 }
--- grails-app/domain/ta/Student.groovy
+++ grails-app/domain/ta/Student.groovy
@@ -5,9 +5,37 @@ import org.grails.datastore.mapping.query.Query
 class Student {
     String name;
     String login;
-    List<Criterion> criterions;
+    List<EvaluationsByCriterion> criterionsAndEvaluations
     static constraints = {
         name blank : false
         login unique : true, blank:false;
+        criterionsAndEvaluations nullable : false;
+    }
+
+    public static void addEvaluation(Evaluation evaluationInstance){
+        for(int i = 0; i< this.criterionsAndEvaluations.size(); i++){
+            if(this.criterionsAndEvaluations.get(i).criterion.description.equals(evaluationInstance.criterion.description)){
+                this.criterionsAndEvaluations.get(i).addEvaluation(evaluationInstance);
+            }
+        }
+    }
+
+    public static EvaluationsByCriterion findEvaluationByCriterion(String criterionName){
+        for(int i =0; i<this.criterionsAndEvaluations.size();i++){
+            if(this.criterionsAndEvaluations.get(i).criterion.description.equals(criterionName)){
+                return this.criterionsAndEvaluations.get(i);
+            }
+        }
+    }
+
+    public static boolean evaluationExist(Evaluation evaluationInstance){
+        for(int i = 0; i<this.criterionsAndEvaluations.size();i++){
+            if(this.criterionsAndEvaluations.get(i).criterion.description.equals(evaluationInstance.criterion.description)){
+                List<Evaluation> evaluationsForThisCriterion = this.criterionsAndEvaluations.get(i).evaluations;
+                for(int j=0; j<evaluationsForThisCriterion.size();j++){
+                    //if()
+                }
+            }
+        }
     }
 }
