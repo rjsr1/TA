68a9c5a73d73c7db8e586a4feef4129a2bb6a0f2
Danilo Lima Ribeiro <dlr4@cin.ufpe.br>
2016-06-25 16:35:13 -0300

    Update AddEvaluationPage.groovy

--- test/functional/pages/AddEvaluationPage.groovy
+++ test/functional/pages/AddEvaluationPage.groovy
@@ -1,6 +1,8 @@
 package pages
 
 import geb.Page
+import ta.Criterion
+
 /**
  * Created by TMB on 20/06/2016.
  */
@@ -8,17 +10,39 @@ class AddEvaluationPage extends Page {
     static url = "/TA/evaluation/create"
 
     static at = {
-        title ==~ /Criar Evaluation/
-    }
-
-    def fillEvaluationDetails(String value, String origin, Date applicationDate,Long criterionId){
-        $("form").origin = origin
-        $("form").value = value
-        $("form").applicationDate = applicationDate
-        $("form").criterion = criterionId;
+        title ==~ /Create Evaluation/
     }
 
     def selectAddEvaluation(){
         $("input", name: "create").click()
     }
+
+    def chooseCriterion(String desc) {
+        //$("select", id: "criterion").click()
+        def la = (String) Criterion.findByDescription(desc).id
+        $("select", id: "criterion").value(la)
+    }
+
+    def chooseValue(String value) {
+        $("select", name: "value").click()
+        $("select", name: "value").find("option").find{it.value().equals(value)}.click()
+    }
+
+    def chooseEvaluationDate(Date evaluationDate){
+        def day = evaluationDate.getDay()
+        def month = evaluationDate.getMonth()
+        def year = evaluationDate.getYear()
+        $("select", name:"applicationDate_day").click()
+        $("select", name: "applicationDate_day").find("option").find{it.value().equals(day)}.click()
+        $("select", name:"applicationDate_month").click()
+        $("select", name: "applicationDate_month").find("option").find{it.value().equals(month)}.click()
+        $("select", name:"applicationDate_year").click()
+        $("select", name: "applicationDate_year").find("option").find{it.value().equals(year)}.click()
+    }
+    def chooseOrigin(String origin){
+        $("select", name:"origin").click()
+        $("select", name: "origin").find("option").find{it.value().equals(origin)}.click()
+    }
+
+
 }
