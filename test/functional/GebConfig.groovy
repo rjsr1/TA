4b14845ede1fb98c8c1449218f613ec896e36090
rjsr@cin.ufpe.br <braitauni463318>
2016-11-05 03:34:27 -0300

    commit teste

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -1,7 +1,7 @@
 import org.openqa.selenium.chrome.ChromeDriver
 
 driver = {
-    File file = new File("C:/SDK/Chromedriver/chromedriver.exe");
+    File file = new File("/home/ess/TA/chromedrivers/chromedriverlinux64");
     System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
     new ChromeDriver();
 }
